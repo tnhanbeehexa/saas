35e187cdf9f73eed36884a51f8efef883bc1e212
Le Phuoc Thanh Nhan <thanh.nhan@beehexa.com>
2023-09-25 16:37:02 +0700

    Nhan-25/09 Footer UI

--- src/frontend/assets/js/app.js
+++ src/frontend/assets/js/app.js
@@ -3,6 +3,7 @@
 import render from "./render.js";
 import formContact from "./views/partials/formContact.js";
 import header from "./views/partials/header.js";
+import footer from "./views/partials/footer.js";
 
 const elementRoot = document.getElementById('root');
 
@@ -10,7 +11,9 @@ const app = () => {
     return render`
         <div class='container-variux'>
             ${header()}
+            ${footer()}
         </div>
+       
     ` 
 };
 
