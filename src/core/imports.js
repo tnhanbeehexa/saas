2c6915bcf09bcb69dd3c5ff838cb9596d7ac7ccb
tnhanbeehexa <thanh.nhan@beehexa.com>
2023-10-17 00:46:39 +0700

    first commit

--- src/core/imports.js
+++ src/core/imports.js
@@ -11,8 +11,8 @@ import showAfter from '../module/triggers/showAfterXSeconds.js';
 import scrollPage from '../module/triggers/showOncePageScroll.js';
 
 export default {
-    clickButton, //trigger
-    existClass, 
+    clickButton,
+    existClass,
     existCookie,
     setOverlay,
     setPosition,
